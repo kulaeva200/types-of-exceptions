d3bf52981c3d379e3591ded6805e19ff829ebb7f
kulaeva200 <118422378+kulaeva200@users.noreply.github.com>
2022-11-22 14:10:06 +0900

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int[] nums = new int[4];
+
+                try
+                {
+                    Console.WriteLine("До генерирования исключения.");  
+                    for (int i = 0; i < 10; i++)
+                    {
+                        nums[i] = i;
+                        Console.WriteLine("nums[(0)]: {1}", i, nums[i]);
+                    }
+                    Console.WriteLine("He подлежит выводу");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Индекс вышел за границы массива!");
+                }
+                Console.WriteLine("После блока перехвата исключения.");
+            }
+        }
+    }
+
